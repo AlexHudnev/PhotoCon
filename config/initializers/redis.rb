dcb7b188169981325226ca532271abcfc979da27
alex <hudneval@mail.ru>
2019-02-18 22:39:21 +0300

    redis debug

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,4 +1,4 @@
 # frozen_string_literal: true
 
-uri = ENV["REDISTOGO_URL"] || "redis://localhost:6379/"
+uri = "redis://redistogo:d14e530dbd0f7f4d7f8ab0ab8ea88694@dory.redistogo.com:9965/"
 REDIS = Redis.new(url: uri)
