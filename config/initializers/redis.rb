073c09611442848973c969ff42055cb1fb462cfa
alex <hudneval@mail.ru>
2019-02-18 22:14:48 +0300

    debug sharing

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,4 +1,5 @@
 # frozen_string_literal: true
 
-uri = URI.parse(ENV['REDIS_URL'] || 'redis://localhost:6379/')
+uri = URI.parse('redis://localhost:6379/')
+uri = URI.parse(ENV['REDIS_URL']) if Rails.env.production?
 REDIS = Redis.new(url: uri)
